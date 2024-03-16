f9d72da1c0c9fe670d8a3d9b612a0946306ac9bb
yogendra291278 <140298009+yogendrav291978@users.noreply.github.com>
2024-03-16 03:25:03 -0700

    Update variables.tf
    Update the Resource group and app name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "echo $AZ_RESOURCE_GROUP"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "echo $APP_NAME"
 }
 
 variable "location" {
